a6e9ae5a77391b293fa3da3fd45dee4850eaf6ef
Adriaan Molendijk <a.l.molendijk@outlook.com>
2021-03-25 17:18:01 +0100

    Added oauth

---
+++ oauth/README.md
@@ -0,0 +1,2 @@
+# oauth
+This folder contains code we used to configure the htpasswd identity provider on the cluster.
---
+++ oauth/setup.sh
@@ -0,0 +1,8 @@
+# Create secret containing .htpasswd file
+oc create secret generic htpass-secret --from-file=htpasswd=.htpasswd -n openshift-config
+
+# Remove kubeadmin user
+oc delete secrets kubeadmin -n kube-system
+
+# Update passwords
+oc create secret generic htpass-secret --from-file=htpasswd=.htpasswd --dry-run=client -o yaml -n openshift-config | oc replace -f -
